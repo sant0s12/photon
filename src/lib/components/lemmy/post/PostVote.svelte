aa82edf22bf6032f1cce8c22b5b8bb467e26413c
sant0s12 <diego.delossantos@mailbox.org>
2023-09-24 12:21:46 +0200

    Warn on vote if not logged in and vote refactor

--- src/lib/components/lemmy/post/PostVote.svelte
+++ src/lib/components/lemmy/post/PostVote.svelte
@@ -4,7 +4,7 @@
   import { ChevronDown, ChevronUp, Icon } from 'svelte-hero-icons'
   import { profile } from '$lib/auth.js'
   import { vote as voteItem } from '$lib/lemmy/contentview.js'
-  import { Button } from 'mono-svelte'
+  import { Button, toast } from 'mono-svelte'
   import { site } from '$lib/lemmy.js'
 
   export let post: Post
@@ -13,21 +13,37 @@
 
   const voteColor = (vote: number, border: boolean = false) =>
     vote == 1 ? `!text-blue-500` : vote == -1 ? `!text-red-500` : ''
+
+  enum VoteType {
+	UpVote,
+	DownVote,
+  };
+
+  const handleVoteClick = async (voteType: VoteType) => {
+	if (!$profile?.jwt) {
+	  toast( { content: "Login first!", type: "warning" } );
+	  return;
+	}
+
+	if (voteType == VoteType.UpVote) {
+	  vote = vote == 1 ? 0 : 1
+	} else {
+	  vote = vote == -1 ? 0 : -1
+	}
+
+    score = await voteItem(post, vote, $profile.jwt)
+  };
 </script>
 
 <slot {vote} {score}>
   <div
     class=" flex items-center text-sm gap-1 rounded-md border border-slate-200
-      dark:border-zinc-800 bg-white dark:bg-zinc-900 px-1 h-8 cursor-pointer"
+      dark:border-zinc-800 bg-white dark:bg-zinc-900 px-1 h-8"
   >
     <Button
       aria-label="Upvote"
       class={vote == 1 ? voteColor(vote) : ''}
-      on:click={async () => {
-        if (!$profile?.jwt) return
-        vote = vote == 1 ? 0 : 1
-        score = await voteItem(post, vote, $profile.jwt)
-      }}
+      on:click={async () => { await handleVoteClick(VoteType.UpVote) }}
       size="square-sm"
       color="tertiary"
       alignment="center"
@@ -47,11 +63,7 @@
         .enable_downvotes
         ? ''
         : 'pointer-events-none opacity-50'}"
-      on:click={async () => {
-        if (!$profile?.jwt) return
-        vote = vote == -1 ? 0 : -1
-        score = await voteItem(post, vote, $profile.jwt)
-      }}
+      on:click={async () => { await handleVoteClick(VoteType.DownVote) }}
       size="square-sm"
       color="tertiary"
     >
